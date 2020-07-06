ae5d68483af73b1a46556b8bad3621d2bf7eddbd
Cloudjuly2020 <Cloudjuly2020@outlook.com>
2020-07-06 15:38:27 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "azurerm" {
+  version = "=2.0.0"
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "${var.prefix}-resources"
+  location = "${var.location}"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "${var.prefix}-network"
+  resource_group_name = "${azurerm_resource_group.example.name}"
+  location            = "${azurerm_resource_group.example.location}"
+  address_space       = ["10.0.0.0/16"]
