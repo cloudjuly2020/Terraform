499135bb5b135fb3f50a434cf3a3bf0511eda4ee
cloudjuly2020 <67755485+cloudjuly2020@users.noreply.github.com>
2020-07-06 16:54:49 +0530

    Update main.tf

--- Demo/main.tf
+++ Demo/main.tf
@@ -3,14 +3,16 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "${var.prefix}-resources"
-  location = "${var.location}"
+# Create a resource group
+resource "azurerm_resource_group" "resource_gp" {
+  name     = "skylines"
+  location = "west Europe"
 }
 
+# Create a virtual network within the resource group
 resource "azurerm_virtual_network" "example" {
-  name                = "${var.prefix}-network"
-  resource_group_name = "${azurerm_resource_group.example.name}"
-  location            = "${azurerm_resource_group.example.location}"
+  name                = "sl"
+  resource_group_name = azurerm_resource_group.resource_gp.name
+  location            = azurerm_resource_group.resource_gp.location
   address_space       = ["10.0.0.0/16"]
-  }
+}
